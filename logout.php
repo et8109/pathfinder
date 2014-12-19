fa6704e0f9d081effdd6eab3178a95f14bac1859
Elliot <ere16@case.edu>
2014-12-19 16:16:51 -0500

    fixing logout

--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once("shared.php");
+require_once("logoutInterface.php");
 
 if(isset($_SESSION['playerID'])){
-    query("UPDATE playerinfo SET zone=0 WHERE id=".prepVar($_SESSION['playerID']));
+    LogoutInterface::logout($_SESSION['playerID']);
 }
 session_destroy();
 echo '<a href="login.php">Back to login</a>'
