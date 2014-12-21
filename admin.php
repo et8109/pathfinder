de188bb2557b9423856f46bc5e5226e9c1253990
Elliot <ere16@case.edu>
2014-12-21 00:09:55 -0500

    created admin interface and moved db creation

--- admin.php
+++ admin.php
@@ -1,3 +1,24 @@
 <?php
-echo "in progress";
+require_once("database.php");
+require_once("adminInterface.php");
+
+try{
+    if(isset($_POST['reset'])){
+        if($_POST['reset'] == "RESET"){
+            AdminInterface::resetDatabase();
+            echo "done db reset";
+        }
+    }
+} catch (Exception $e){
+    echo $e->getMessage();
+}
 ?>
+<html>
+    <head>
+    </head>
+    <body>
+        <form action="admin.php" method="post">
+            type RESET to reset db: <input type=text name=reset maxlength=5></input>
+        <input type=submit></input>
+    </body>
+</html>
