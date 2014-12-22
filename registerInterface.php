b48f006960c1a434b169027105fb72ad5e6e6b08
Elliot <ere16@case.edu>
2014-12-21 19:29:57 -0500

    added default audiourl on registration

--- registerInterface.php
+++ registerInterface.php
@@ -7,7 +7,7 @@ class RegisterInterface extends Interface_class {
     public static function register($uname, $pass){
         $uname = self::prepVar($uname);
         $pass = self::prepVar($pass);
-        self::$db->querySingle("insert into playerinfo (uname, pass, zone, posx, posy, audioURL, peerid, health, kills) values ($uname, $pass,0,0,0,'aurl','123qwe',3,0)");
+        self::$db->querySingle("insert into playerinfo (uname, pass, zone, posx, posy, audioURL, peerid, health, kills) values ($uname, $pass,0,0,0,'attack.mp3','123qwe',3,0)");
     }
 }
 ?>
