aa05749db2b54f678e5ef06e89ff151b91dca2b1
Elliot <ere16@case.edu>
2014-12-26 17:12:34 -0500

    added audio table to db

--- interfaces/adminInterface.php
+++ interfaces/adminInterface.php
@@ -75,14 +75,36 @@ class AdminInterface extends Interface_class{
                            "kills int(3),".
                            "PRIMARY KEY (id)".
                            ")");
+        //individual audio info
+        self::$db->querySingle("CREATE TABLE audio (".
+                               "id int(3),".
+                               "objid varchar(4),".
+                               "url varchar(10),".
+                               "length int,".
+                               "PRIMARY KEY (objid, id)".
+                               ")");
         //populate databases
-        self::$db->querySingle("INSERT INTO ambient (zone, posx, posy, audioURL) values (1, 10, 10, 'Birds.mp3')");
+        self::$db->querySingle("INSERT INTO ambient (zone, posx, posy, audioURL) values (0, 10, 10, 'Birds.mp3')");
         self::$db->querySingle("INSERT INTO enemies (id, posx, posy, zone, health, lastAudio, finish, start) values (0, 20, 20, 1, 4, 1, 0, 0)");
         self::$db->querySingle("INSERT INTO enemyInfo (id, audioURL) values (0, 'Growl.mp3,Chomp.mp3,ed.mp3')");
         self::$db->querySingle("INSERT INTO movement (zone, audioURL) values (1, 'carpetStep.wav')");
         self::$db->querySingle("INSERT INTO npcs (id, zone, posx, posy, audioURL, lastAudio, finish, start) values (0, 1, 5, 10, 'wc.mp3,Kntq.mp3,Knty.mp3,Kntn.mp3', 0, 0, 0)");
         self::$db->querySingle("INSERT INTO playerinfo (id, uname, pass, posx, posy, zone, peerid, health, audioURL, kills) values ".
                                "(1, 'guest', 'guest', 1, 1, 0, 'abcd1234', 3, 'attack.mp3', 0)");
+        self::$db->querySingle("INSERT INTO audio (objid, id, url, length) values ".
+                               "(a1, 0, 'Birds.mp3', 2),".//ambient
+                               "(e0, 0, 'Growl.mp3', 7),".//enemies
+                               "(e0, 1, 'Chomp.mp3', 2),".
+                               "(e0, 2, 'ed.mp3', 2),".
+                               "(m1, 0, 'carpetStep.wav', 6),".//movement
+                               "(n0, 0, 'wc.mp3', 3),".//npcs
+                               "(n0, 1, 'Kntq.mp3', 5),".
+                               "(n0, 2, 'Knty.mp3', 6),".
+                               "(n0, 3, 'Kntn.mp3', 5),".
+                               "(p1, 0, 'attack.mp3', 3)".//player
+                               "(s0, 0, 'Lowlife.mp3', 4),".//sprite
+                               "(s0, 1, 'Dead.mp3', 4)"
+                               );
     }
 }
 ?>
