9ee269918e05e2047dbecde5b247d25e73660a60
Elliot Essman <et8109@gmail.com>
2015-03-18 14:23:48 -0400

    added zone change func to check boundaries

--- server/communication/changeZones.php
+++ server/communication/changeZones.php
@@ -7,62 +7,58 @@ require_once("shared/Translator.php");
 
 try{
 //only posts should be accepted. other verbs are ignored.
-if(!empty($_POST)){
-    //setup
-    //AudioObj::initState();//sets up globals: time and json array
-    //get player db info
-    $player = Player::fromDatabase($_SESSION['playerID']);
-    $zone= $player->zone;
-    $dir = $_POST['dir'];
-    //set new zone co-ords
-    $newZone = null;
-    switch($dir){
-    case 'N':
-        if($zone->zoney >= constants::numZonesSrt){
-            throw new Exception("out of map range");
-        }
-        $newZone = new Zone($zone->zonex,
-            $zone->zoney + 1);
-        break;
-    case 'S':
-        if($zone->zoney <= 0){
-            throw new Exception("out of map range");
-        }
-        $newZone = new Zone($zone->zonex,
-            $zone->zoney - 1);
-        break;
-    case 'E':
-        if($zone->zonex >= constants::numZonesSrt){
-            throw new Exception("out of map range");
-        }
-        $newZone = new Zone($zone->zonex + 1,
-            $zone->zoney);
-        break;
-    case 'W':
-        if($zone->zonex <= 0){
-            throw new Exception("out of map range");
-        }
-        $newZone = new Zone($zone->zonex - 1,
-            $zone->zoney);
-        break;
-    case 'init':
-        //initial zone, just needs to load
-        $newZone = $zone;
-        break;
-    default:
-        throw new Exception("unknown direction: $dir");
-    }
-    //continue to change zone
-    $player->reposition($newZone);
-    //prep audio for things in the zone
-    Npc::getPrepInfo($newZone);
-    Enemy::getPrepInfo($newZone);
-    echo Translator::send();
-
+    if(!empty($_POST)){
+        //get player db info
+        $player = Player::fromDatabase($_SESSION['playerID']);
+        $newZone = getNewZone($player->zone, $_POST['dir']);
+        //move player
+        $player->reposition($newZone);
+        //prep audio for things in the zone
+        Npc::getPrepInfo($newZone);
+        Enemy::getPrepInfo($newZone);
+        
+        echo Translator::send();
     } else{
         throw new Exception("unknown verb");
     }
 } catch(Exception $e){
     require_once("shared/ErrorHandler.php");
     echo ErrorHandler::handle($e);
+}
+
+/**
+ * Returns the new zone from the old zone and movement direction
+ */
+function getNewZone($prev, $dir){
+    switch($dir){
+    case 'N':
+        if($prev->zoney >= constants::numZonesSrt){
+            throw new Exception("out of map range");
+        }
+        return new Zone($prev->zonex,
+                        $prev->zoney + 1);
+    case 'S':
+        if($prev->zoney <= 0){
+            throw new Exception("out of map range");
+        }
+        return new Zone($prev->zonex,
+                        $prev->zoney - 1);
+    case 'E':
+        if($prev->zonex >= constants::numZonesSrt){
+            throw new Exception("out of map range");
+        }
+        return new Zone($prev->zonex + 1,
+                        $prev->zoney);
+    case 'W':
+        if($prev->zonex <= 0){
+            throw new Exception("out of map range");
+        }
+        $newZone = new Zone($prev->zonex - 1,
+                            $prev->zoney);
+    case 'init':
+        //initial zone, just needs to load
+        return $prev;
+    default:
+        throw new Exception("unknown direction: $dir");
+    }
 }
