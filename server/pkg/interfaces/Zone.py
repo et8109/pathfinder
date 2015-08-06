414ede191bf42f5dd03999d19772b983377da2c5
Elliot Essman <et8109@gmail.com>
2015-08-06 10:27:07 -0400

    Update Zone.py

--- server/pkg/interfaces/Zone.py
+++ server/pkg/interfaces/Zone.py
@@ -1,17 +1,20 @@
 from pkg.communication.Overseer import *
-from pkg.database.database import Zone_table
+from pkg.database.tables import zones as Zone_table
 
 class Zone():
 
-    def __init__(self, zid):
+    def __init__(self, zid, up, down, left, right):
         self.zid = zid
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
 
     @staticmethod
     def from_id(zid):
-        #zone = Zone_table.from_id(zid)
-        return Zone(zid)
+        zone = Zone_table.from_id(zid)
+        return Zone(zid, zone.up, zone.down, zone.left, zone.right)
 
     #called when a player enters the scene
     def onEnter(self, player):
-        print("--> in zone")
         Overseer.Overseer.sendData("entered zone", player.pid) 
