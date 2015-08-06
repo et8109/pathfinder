c40999ebf2d3082f50c2a873617669d9bcacbbf3
Elliot Essman <et8109@gmail.com>
2015-08-06 12:56:37 -0400

    Create npcs.py

---
+++ server/pkg/database/tables/npcs.py
@@ -0,0 +1,22 @@
+from .Table import Table, Table.query as query
+
+class Npcs(Table):
+
+    @staticmethod
+    def create():
+        query("""CREATE TABLE npcs (
+            id int(3) NOT NULL AUTO_INCREMENT,
+            zoneid int(3),
+            PRIMARY KEY (id),
+            FOREIGN KEY (zoneid) REFERENCES zones(id)
+            )"""
+            )
+
+    @staticmethod
+    def init():
+        query(
+                """INSERT INTO npcs (zoneid) 
+                values (%s)
+                """,
+                (1)
+                )
